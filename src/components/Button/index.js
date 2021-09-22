5a64d487ee69e8978885f7a0d1d044b941474260
Brian Grapes <bgrapes@gmail.com>
2021-09-22 14:35:59 -0700

    style(formatting): format files with prettier

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,15 +1,8 @@
 import React from 'react'
-import {
-  ButtonStyled
-} from './styles'
+import { ButtonStyled } from './styles'
 
-const Button = ({
-  children,
-  large
-}) => {
-  return (
-  	<ButtonStyled large={large}>{children}</ButtonStyled>
-  )
+const Button = ({ children, large }) => {
+  return <ButtonStyled large={large}>{children}</ButtonStyled>
 }
 
 export default Button
